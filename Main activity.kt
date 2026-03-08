43a0b8668f198fefaf651b877be574250d9063a8
David Sosnac <105160225+Sosnac@users.noreply.github.com>
2026-03-08 13:30:37 +0200

    Create Main activity.kt

---
+++ Main activity.kt
@@ -0,0 +1,31 @@
+package com.predictora
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.predictora.ui.theme.PredictorATheme
+import com.predictora.ui.navigation.PredictorANavGraph
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContent {
+            PredictorATheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    PredictorANavGraph()
+                }
+            }
+        }
+    }
+}
